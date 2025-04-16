5d3beff0e41afbee34a8c9b08403344a1ba4c31f
faf1997 <alejandrofranfio@gmail.com>
2025-04-16 10:14:43 -0300

    first commit

---
+++ fix_dev_report/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Módulo para arreglar los reportes en local",
+    "version": "1.0.0",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "category": "Dev",
+    "data": [
+        "data/data.xml",
+    ],
+}
